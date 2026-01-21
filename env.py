510718a68546d36342e5ba4d525ed00e284f0541
florerrssll <florersl11@gmail.com>
2026-01-21 10:45:35 +0100

    fonctions env process

--- env.py
+++ env.py
@@ -1,3 +1,5 @@
+import os
+import random
 import threading 
 import socket
 import signal
@@ -5,7 +7,7 @@ import multiprocessing
 from multiprocessing import shared_memory, Semaphore, Queue 
 
 #structure représntation population
-Class Population:
+class Population:
 
     def __init__(self, name, size, energy, is_active):
         self.name = name
@@ -14,20 +16,9 @@ Class Population:
         self.is_active = is_active
 
 
-#structure état global
-Class GlobalState:
-
-    def __init__(self):
-        self.nb_preys = 0
-        self.nb_predators = 0
-        self.grass = 1000  # Quantité initiale d'herbe
-        self.H = 50  
-        self.R = 10  
-        self.drought = False  
-
 #structure environnement 
 
-Class EnvProcess:
+class EnvProcess:
     def __init__(self):
         self.serve = True #Variable de contrôle boucle principale
         self.shared_state = SharedState() #état global partagé (données que les autres processus peuvent consulter et modifier)
@@ -48,27 +39,65 @@ Class EnvProcess:
         connection_thread.start()
         signal_thread.start()
 
+        #planifie une sécheresse aléatoire 
+        self.schedule_random_drought()
+
         connection_thread.join()
         signal_thread.join()
 
         self.cleanup() #ferme les connexions et libère les ressources partagées
 
+    def schedule_random_drought(self):
+        #Génère un délai aléatoire entre 15 et 60 secondes
+        delay_seconds = random.randint(15, 60)
 
+        drought_timer = threading.Timer(delay_seconds, self.trigger_drought)
+        drought_timer.start()
+
+    def trigger_drought(self):
+        if self.serve:
+            os.kill(os.getpid(), signal.SIGUSR1)  # Envoyer le signal SIGUSR1
+            
+    def handle_signals(self):
+        def drought_handler(signum, frame):
+            with self.sem_mutex:
+                self.shared_state.drought = True
+                self.shared_state.grass *= 0.5
+                if self.serve:
+                    self.schedule_random_drought()  # Planifier une nouvelle sécheresse
+
+        signal.signal(signal.SIGUSR1, drought_handler)
+
+        while self.serve:
+            signal.pause()  # Attend un signal
+    
     def handle_connections(self):
         while self.serve:
             client_socket, addr = self.server_socket.accept()
             self.process_client_message(client_socket, addr)
     
     def process_client_message(self, client_socket, addr):
+            with client_socket:
+                data = client_socket.recv(1024).decode()
+                if data == "stop":
+                    self.serve = False  # Arrêter le serveur
+                elif data == "je mange":
+                    self.update_grass(-1)  # Décrémenter l'herbe
+                elif data == "je meurs":
+                    self.update_population(-1)  # Décrémenter la population
+                elif data == "je me reproduis":
+                    self.update_population(1)  # Incrémenter la population
 
 
-    def handle_signals(self):
-        def drought_handler(signum, frame):
-            
+
+        
     
 
     def update_grass(self, delta):
     #delta = variation de l'herbe
+        with self.sem_mutex:
+            self.shared_state.grass += delta
+            self.sem_grass.release() if delta > 0 else  None #Met à jour le sémaphore herbe
     
     def update_populations(self, delta):
         with self.sem_mutex:
